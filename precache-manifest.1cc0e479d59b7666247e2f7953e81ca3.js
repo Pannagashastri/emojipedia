161381cc7730a179adb6e6ed61723e0d14d47b69
shashidhar843 <38162619+shashidhar843@users.noreply.github.com>
2020-11-08 21:23:06 +0530

    Updates

---
+++ precache-manifest.1cc0e479d59b7666247e2f7953e81ca3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58bed6b086d89b4328ca85571233c32c",
+    "url": "/emojipedia/index.html"
+  },
+  {
+    "revision": "d478e7f699f8a4fe0e4f",
+    "url": "/emojipedia/static/js/2.fc726699.chunk.js"
+  },
+  {
+    "revision": "c6386c2562e933e57c0b",
+    "url": "/emojipedia/static/js/main.2c205311.chunk.js"
+  },
+  {
+    "revision": "7ea935b83df6c53ba113",
+    "url": "/emojipedia/static/js/runtime-main.d52a7a0e.js"
+  }
+]);
